b5ee02de772fe7cda99ae7e5a3dfe5d4fe73231d
Marek Dado <marekdado1010@gmail.com>
2019-04-29 12:18:05 +0200

    Vytvorenie api na login, vytvorenie obrazovky Home a Login, uspesne vytvaranie tokenu

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,71 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+//const cors = require('cors')({ origin: true });
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const database = admin.database().ref('/packets');
+const login = admin.database().ref('/users');
+
+exports.auth = functions.https.onRequest((req, res) => {
+
+    if(req.method === 'POST') {
+
+        var username = req.query.username;
+        var password = req.query.password;
+
+        console.log(username);  //testovanie
+        console.log(password);
+
+        if(username) {
+            if (password) {
+
+                return login.orderByChild('username').equalTo(username).once('value', (snapshot) => {
+
+                    if (snapshot.val()) {
+
+                        console.log(snapshot.val());
+
+                        var obj = snapshot.val();
+                        var key = Object.keys(obj)[0];
+                        var user = obj[key];
+
+                        if (user.password === password) {
+
+                            console.log("User " + user);
+                            console.log("Key " + key);
+                            console.log(user.username);
+                            console.log(user.password);
+
+                            return admin.auth().createCustomToken(user.username).then((token) => {
+                                console.log(token);
+                                snapshot.child(key).ref.update({
+                                    "token": token
+                                }).catch(function(error) {
+                                    console.log("Marek " + error);
+                                });
+
+                                res.status(200).send(token);
+                            });
+
+                        } else {
+                            res.status(403).send('Incorrect password');
+                        }
+                    } else {
+                        res.status(403).send('Incorrect username');
+                    }
+                });
+            } else {
+                res.status(400).send('Missing parameter');
+            }
+        } else {
+            res.status(400).send('Missing parameter');
+        }
+    } else {
+        res.status(400).send();
+    }
+});
+
+
+
+
---
+++ screens/Login.js
@@ -0,0 +1,78 @@
+import { Alert, View, Text, TextInput, TouchableOpacity,StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import fb from '../firebase';
+
+export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    _login = async () => {
+        await fb.instance.login(this.state.username, this.state.password).then(tok => {
+
+            if(tok != null) {
+                fb.instance.token = tok;
+                fb.instance.author = this.state.username;
+                console.log("Token: " + fb.instance.token);  //testovanie
+            }
+        })
+
+    };
+
+    render() {
+
+        return (
+            <View style = {styles.container}>
+                <TextInput style = {styles.input}
+                           autoCapitalize="none"
+                           onChangeText = {(text_user) => this.setState({username: text_user})}
+                           autoCorrect={false}
+                           placeholder='Username'
+                           placeholderTextColor='grey'/>
+                <TextInput style = {styles.input}
+                           onChangeText = {(passwd_user) => this.setState({password: passwd_user})}
+                           placeholder='Password'
+                           placeholderTextColor='grey'
+                           secureTextEntry/>
+
+                <TouchableOpacity style={styles.buttonContainer}
+                                  onPress={this._login}
+                >
+                    <Text style={styles.buttonText}>LOGIN</Text>
+                </TouchableOpacity>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 200
+    },
+    input:{
+        marginLeft: 30,
+        marginRight: 30,
+        height: 40,
+        backgroundColor: 'rgba(225,225,225,0.2)',
+        marginBottom: 10,
+        padding: 10,
+        color: 'grey'
+    },
+    buttonContainer:{
+        marginLeft: 30,
+        marginRight: 30,
+        backgroundColor: 'grey',
+        paddingVertical: 15
+    },
+    buttonText:{
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: '700'
+    }
+});
